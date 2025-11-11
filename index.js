0d236d08a8b6a6fa32d9d28008ed97816ff7d04c
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-11-11 16:52:26 +0600

    mongo connected

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+
+
+
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const cors = require("cors");
+require("dotenv").config();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
+
+// mongo connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@chat-application.qhq6ecs.mongodb.net/?appName=chat-application`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
